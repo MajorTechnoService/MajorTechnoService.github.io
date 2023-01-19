96281e07f46322bb935d43095326a7eb48e3ded2
Davide De Simone <d.desimone@iperurania.it>
2023-01-19 15:05:43 +0100

    Add cookies policy

--- components/footer.js
+++ components/footer.js
@@ -6,8 +6,13 @@ class Footer extends HTMLElement {
       fetch('/components/Footer.html')
         .then( r => r.text() )
         .then( t => this.innerHTML = t )
-      
+        .finally( () => $( "#year" ).text(new Date().getFullYear()))
     }
   }
   
   customElements.define('footer-component', Footer);
+
+
+
+
+
--- components/header.js
+++ components/header.js
@@ -1,9 +1,15 @@
 class Header extends HTMLElement {
-
+  constructor() {
+    super();
+  }
   connectedCallback() {
     fetch('/components/Header.html')
       .then( r => r.text() )
       .then( t => this.innerHTML = t )
+      $('html').addClass('hidden');
+      $(document).ready(function() {    // EDIT: From Adam Zerner's comment below: Rather use load: $(window).on('load', function () {...});
+        $('html').show();  // EDIT: Can also use $('html').removeClass('hidden'); 
+       }); 
     }
   }
   
